7d9e51a0aad790803d9306b0c005135096d71e1a
jdograph <jdograph@gmail.com>
2026-07-03 10:17:47 +0200

    Remove comments from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'https://esm.sh/react@18'
 import ReactDOM from 'https://esm.sh/react-dom@18/client'
-import App from './App.jsx' // On importe le composant principal
-import './index.css' // On importe le CSS global
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+)
